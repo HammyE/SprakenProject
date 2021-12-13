7131d9e5ebf9dc7f6bcadcadc4c81c1d45a50e75
HammyE <erik.oj.hamberg@gmail.com>
2021-12-13 17:07:28 +0100

    Added skeleton files

--- test_DataParser.py
+++ test_DataParser.py
@@ -1,5 +1,7 @@
 from unittest import TestCase
 
+import numpy as np
+
 from DataParser import DataParser
 
 
@@ -7,23 +9,23 @@ class TestDataParser(TestCase):
     def test_parse_train(self):
         parser = DataParser("testDataset.txt")
         features, labels = parser.parse_train()
-        correct_features = [[0, 0, 1],
-                            [0, 1, 0],
-                            [1, 0, 1],
-                            [1, 1, 0],
-                            [1, 0, 1]]
-        correct_labels = ["Trump2020", "Biden2020", "Trump2020", "Biden2020", "Biden2020"]
+        correct_features = np.array([[0, 0, 1],
+                                     [0, 1, 0],
+                                     [1, 0, 1],
+                                     [1, 1, 0],
+                                     [1, 0, 1]])
+        correct_labels = np.array(["Trump2020", "Biden2020", "Trump2020", "Biden2020", "Biden2020"])
         TestCase.assertEqual(self, features, correct_features)
         TestCase.assertEqual(self, labels, correct_labels)
 
     def test_parse_test(self):
         parser = DataParser("testDataset.txt")
         tweets, hashtags = parser.parse_test()
-        correct_tweets = ["Donald Trump is my best friend",
+        correct_tweets = (["Donald Trump is my best friend",
                           "Joe Biden is so hot lol",
                           "Why can't libtards just understand that we want to MAGA",
                           "Trans rights are human rights!",
-                          "I love how a president can rape women and get away with it"]
-        correct_hashtags = ["Trump2020", "Biden2020", "Trump2020", "Biden2020", "Biden2020"]
+                          "I love how a president can rape women and get away with it"])
+        correct_hashtags = np.arary(["Trump2020", "Biden2020", "Trump2020", "Biden2020", "Biden2020"])
         TestCase.assertEqual(self, tweets, correct_tweets)
         TestCase.assertEqual(self, hashtags, correct_hashtags)
