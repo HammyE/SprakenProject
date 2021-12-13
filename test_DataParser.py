f108961fca3f9582a0efe4dd12bcb5912464b9f1
HammyE <erik.oj.hamberg@gmail.com>
2021-12-13 17:04:17 +0100

    Added skeleton files

--- test_DataParser.py
+++ test_DataParser.py
@@ -19,11 +19,11 @@ class TestDataParser(TestCase):
     def test_parse_test(self):
         parser = DataParser("testDataset.txt")
         tweets, hashtags = parser.parse_test()
-        correct_tweets = ["Donald Trump is my best friend #Trump2020",
-                          "Joe Biden is so hot lol #Biden2020",
-                          "Why can't libtards just understand that we want to MAGA #Trump2020",
-                          "Trans rights are human rights! #Biden2020",
-                          "I love how a president can rape women and get away with it #Biden2020"]
+        correct_tweets = ["Donald Trump is my best friend",
+                          "Joe Biden is so hot lol",
+                          "Why can't libtards just understand that we want to MAGA",
+                          "Trans rights are human rights!",
+                          "I love how a president can rape women and get away with it"]
         correct_hashtags = ["Trump2020", "Biden2020", "Trump2020", "Biden2020", "Biden2020"]
         TestCase.assertEqual(self, tweets, correct_tweets)
         TestCase.assertEqual(self, hashtags, correct_hashtags)
