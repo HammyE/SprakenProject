cc989ff3e5be51296bce7986eaa87ba5a15cf98a
bovengan <98danbou11@gmail.com>
2021-12-14 03:40:52 +0100

    Lagt till loss funktionen och plottandet, skapade även en testfil

--- HashtagTrainer.py
+++ HashtagTrainer.py
@@ -1,7 +1,13 @@
+import math
+import random
 import numpy as np
 import pandas
+from matplotlib import pyplot as plt
+import matplotlib
+matplotlib.use("TkAgg")
 
-class HashtagTrainer:
+
+class HashtagTrainer(object):
     """
     This class performs multinomial logistic regression using batch gradient descent
     or stochastic gradient descent
@@ -42,7 +48,7 @@ class HashtagTrainer:
             # Number of labels.
             self.LABELS = len(set(y))
 
-            # Set of labels.
+            # Distinct labels.
             self.labels = list(dict.fromkeys(y))
 
             # Encoding of the data points (as a DATAPOINTS x FEATURES size array).
@@ -55,8 +61,25 @@ class HashtagTrainer:
             # self.theta = np.random.uniform(-1, 1, (self.LABELS, self.FEATURES))
             self.theta = np.ones((self.LABELS, self.FEATURES))
 
-            # The current gradient.
+            # The current jacoby matrix.
             self.jacoby = np.zeros((self.LABELS, self.FEATURES))
+
+    def loss(self, x, y):
+        """
+        Computes the loss function given the input features x and labels y
+
+        :param      x:    The input features
+        :param      y:    The correct labels
+        """
+        the_sum = 0
+
+        for k in range(0, self.LABELS):
+            for d in range(0, self.DATAPOINTS):
+                if self.labels[k] == y[d]:
+                    the_sum += -math.log((self.softmax(np.dot(self.theta, x[d])))[k])
+
+        final_sum = the_sum / self.DATAPOINTS
+        return final_sum
 
     def softmax(self, z):
         """
@@ -64,34 +87,40 @@ class HashtagTrainer:
         """
         return np.exp(z) / sum(np.exp(z))
 
-    def compute_gradient_for_all(self):
+    def compute_jacobian(self):
         """
-        Computes the gradient based on the entire dataset
+        Computes the jacobian matrix based on the entire dataset
         (used for batch gradient descent).
         """
         for k in range(0, self.LABELS):
-            v_1 = 0
+            k_label_sum = 0
             for d in range(0, self.DATAPOINTS):
-                v_2 = (self.softmax(np.dot(self.theta, self.x[d])))[k]
+                hv = (self.softmax(np.dot(self.theta, self.x[d])))[k]
                 if self.labels[k] == self.y[d]:
-                    v_1 += self.x[d] * (v_2 - 1)
+                    k_label_sum += self.x[d] * (hv - 1)
                 else:
-                    v_1 += self.x[d] * v_2
-            v_1 = v_1 / self.DATAPOINTS
+                    k_label_sum += self.x[d] * hv
+            k_label_avg = k_label_sum / self.DATAPOINTS
             for n in range(0, self.FEATURES):
-                self.jacoby[k][n] = v_1[n]
+                self.jacoby[k][n] = k_label_avg[n]
 
     def fit(self):
         """
         Performs Batch Gradient Descent
         """
+        self.init_plot(self.FEATURES)
         while True:
-            self.compute_gradient_for_all()
+            self.compute_jacobian()
 
+            # Time to update the weights, we'll go in opposite direction of the steepest route
+            # meaning we're locating the minimum
             for k in range(1, self.LABELS):
                 for n in range(0, self.FEATURES):
                     self.theta[k][n] = self.theta[k][n] - self.LEARNING_RATE * self.jacoby[k][n]
 
+            self.update_plot(self.loss(self.x, self.y))
+
+            # When the step is small enough it means we're almost at the minimum
             if np.sum(self.jacoby ** 2) < self.CONVERGENCE_MARGIN:
                 break
 
@@ -111,15 +140,62 @@ class HashtagTrainer:
             results[predicted_index][true_index] = results[predicted_index][true_index] + 1
             print(probability_vector)
 
+        # Andel av gissningarna som blir rätt
+        correct_ones = 0
+        for i in range(self.LABELS):
+            correct_ones += results[i][i]
+        accuracy = (correct_ones / np.sum(results)) * 100
+        print("Accuracy: " + str(accuracy) + "%")
+
         row_labels = [self.labels[0], self.labels[1], self.labels[2], self.labels[3]]
         column_labels = [self.labels[0], self.labels[1], self.labels[2], self.labels[3]]
         df = pandas.DataFrame(results, columns=column_labels, index=row_labels, dtype=int)
         print(df)
 
+    def update_plot(self, *args):
+        """
+        Handles the plotting
+        """
+
+        if self.i == []:
+            self.i = [0]
+        else:
+            self.i.append(self.i[-1] + 1)
+
+        for index, val in enumerate(args):
+            self.val[index].append(val)
+            self.lines[index].set_xdata(self.i)
+            self.lines[index].set_ydata(self.val[index])
+
+        self.axes.set_xlim(0, max(self.i) * 1.5)
+        self.axes.set_ylim(0, max(max(self.val)) * 1.5)
+
+        plt.draw()
+        plt.pause(1e-20)
+
+    def init_plot(self, num_axes):
+        """
+        num_axes is the number of variables that should be plotted.
+        """
+        self.i = []
+        self.val = []
+        plt.ion()
+        self.axes = plt.gca()
+        self.lines = []
+
+        for i in range(num_axes):
+            self.val.append([])
+            self.lines.append([])
+            self.lines[i], = self.axes.plot([], self.val[0], '-', c=[random.random() for _ in range(3)], linewidth=1.5,
+                                            markersize=4)
+
+
 def main():
     """
     Tests the code on a toy example.
     """
+    # --------------------- Training ---------------------- #
+    # The training-tweets coded in to their features
     x = [[1, 1, 0, 0, 0, 0, 0, 0],
          [0, 1, 0, 0, 0, 0, 0, 0],
          [1, 0, 0, 0, 0, 0, 0, 0],
@@ -137,17 +213,28 @@ def main():
          [0, 0, 0, 0, 0, 0, 0, 1],
          [1, 0, 0, 0, 0, 0, 1, 0]]
 
-    #  Encoding of the correct classes for the training material
+    # The training-tweets' hashtags/labels/classes
     y = ["glad", "glad", "glad", "glad", "arg", "arg", "arg", "arg", "ledsen", "ledsen", "ledsen", "ledsen",
          "exalterad", "exalterad", "exalterad", "exalterad"]
+
+    # Create a HashtagTrainer object with the tweets and their hashtags
     b = HashtagTrainer(x, y)
+
+    # Train the model
     b.fit()
+
+    # --------------------- Testing ---------------------- #
+    # These test-tweets:
+    # "vi alla vill gå på denna fest och hoppas på att få le och skratta #glad"
+    # "jag är så taggad och uppspelt att jag tror jag kommer böla #exalterad"
+    # Has been coded into their features
     test_data = [[1, 1, 1, 0, 0, 0, 0, 0, 0],
                  [1, 0, 0, 0, 0, 0, 1, 1, 1]]
-    arr = np.array(test_data)
+
+    # The test-tweet's hashtags
     test_labels = ["glad", "exalterad"]
-    b.classify_datapoints(arr, test_labels)
-    # b.print_result()
+
+    b.classify_datapoints(test_data, test_labels)
 
 
 if __name__ == '__main__':
